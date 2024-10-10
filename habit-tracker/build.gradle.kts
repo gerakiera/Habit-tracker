9e173ccbebbe0abd09eb4003318c10c549122304
Герасименя Кристина <gerasimenyakristina@yandex.ru>
2024-10-10 15:36:52 +0300

    add assert and junit

--- habit-tracker/build.gradle.kts
+++ habit-tracker/build.gradle.kts
@@ -20,6 +20,9 @@ dependencies {
     annotationProcessor ("org.projectlombok:lombok:1.18.34")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.assertj:assertj-core:3.6.1")
+    testImplementation ("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly ("org.junit.jupiter:junit-jupiter-engine:5.8.1")
 }
 
 tasks.test {
