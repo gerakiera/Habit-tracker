68050800628de9da2841d27f2dc84eb6ca591467
Герасименя Кристина <gerasimenyakristina@yandex.ru>
2024-10-10 13:38:07 +0300

    menu rewritten in eng/add admin acc creation

--- habit-tracker/src/main/java/org/example/service/UserService.java
+++ habit-tracker/src/main/java/org/example/service/UserService.java
@@ -7,39 +7,49 @@ import java.util.Map;
 import java.util.UUID;
 
 public class UserService {
-    private final Map<String, User> users = new HashMap<>();
+    private final Map<String, User> users;
+
+    public UserService(Map<String, User> users) {
+        this.users = users;
+    }
+
+    public User createAdmin(String adminName, String adminEmail, String adminPassword) {
+        User admin = new User(adminName, adminEmail,adminPassword);
+        users.put(adminEmail, admin);
+        return admin;
+    }
 
     public String registerUser(String name, String email, String password) {
         if(users.containsKey(email)) {
-            return "Пользователь с данными " + email + " уже существует.";
+            return "A user with the data “ + email + ” already exists.";
         }
         User newUser = new User(name, email, password);
         users.put(email, newUser);
-        return "Пользователь зарегистрирован.";
+        return "User registered.";
     }
 
     public String loginUser(String email, String password) {
         User user = users.get(email);
 
         if(user == null) {
-            return "Пользователь не найден.";
+            return "User not found.";
         }
 
         if(!user.getPassword().equals(password)) {
-            return "Неверный пароль.";
+            return "Wrong password.";
         }
 
-        return "Авторизация успешна.";
+        return "Authorization successful.";
     }
 
     public String editUser(String email, String newName, String newEmail, String newPassword) {
         User user = users.get(email);
         if(user == null) {
-            return "Пользователь не найден.";
+            return "User not found.";
         }
 
         if(users.containsKey(newEmail)) {
-            return "Email уже используется.";
+            return "Email is already in use.";
         }
 
         user.setName(newName);
@@ -50,27 +60,27 @@ public class UserService {
             users.remove(email);
             users.put(newEmail, user);
         }
-        return "Данные обновлены.";
+        return "Data updated.";
     }
 
     public String deleteUser(String email) {
         if(users.get(email) == null) {
-            return "Пользователь не найден.";
+            return "User not found.";
         }
 
         users.remove(email);
-        return "Пользователь удалён.";
+        return "User deleted.";
     }
 
     public String resetPassword(String email) {
         User user = users.get(email);
         if(user == null) {
-            return "Пользователь не найден.";
+            return "User not found.";
         }
 
         String randomPassword = UUID.randomUUID().toString();
         user.setPassword(randomPassword);
 
-        return "Пароль сброшен, новый направлен на email.";
+        return "Password reset, new one sent to email.";
     }
 }
